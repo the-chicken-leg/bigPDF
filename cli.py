a964a372550d818a220ffd334deabeae96107f8c
the-chicken-leg <michael.k.koch3@gmail.com>
2024-11-15 10:16:33 +0100

    minor cli changes

--- cli.py
+++ cli.py
@@ -1,14 +1,9 @@
-from pathlib import Path
-from tkinter import *
-from tkinter import ttk
-from tkinter.filedialog import asksaveasfilename
-
 from directories import DIRECTORIES
-from big_pdf import create_big_pdf
 from gui import get_filename_and_save
 
-input_directory = input('Which big PDF do want to create? Enter "ck", "idms", or "tomo": ')
+input_directory = None
 while not input_directory in DIRECTORIES:
-    input_directory = input('Which big PDF do want to create? Enter "ck", "idms", or "tomo": ')
+    input_directory = input('\nWhich big PDF do want to create? Enter "ck", "idms", or "tomo": ')
 
+print("Select save location...")
 get_filename_and_save(input_directory)
