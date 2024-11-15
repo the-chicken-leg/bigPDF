57f6e30915d8e30577d5406eeab9074eaff6ff0e
the-chicken-leg <michael.k.koch3@gmail.com>
2024-11-15 12:21:06 +0100

    added contents output file

--- big_pdf.py
+++ big_pdf.py
@@ -4,30 +4,38 @@ from pypdf import PdfWriter
 
 from directories import DIRECTORIES
 
-def create_big_pdf(glob_dir, output_path):
+def create_big_pdf(glob_dir, pdf_output_path):
     writer = PdfWriter()
 
     glob_sort = sorted(
         glob_dir.glob("**/*-SVC-*.pdf"),
-        key=lambda path: path.name
+        key=lambda filepath: filepath.name
     )
-    glob_dedup = {path.name: path for path in glob_sort}
+    glob_dedup = {filepath.name: filepath for filepath in glob_sort}
     glob_dedup.pop("C-SVC-00186 CK Pre-Upgrade Configuration Checklist, 8.x-9.x to 10.x.pdf", None)     # this file destroys everything
 
-    # for path in islice(glob_dedup.values(), 10):        # use for testing slices
-    for path in glob_dedup.values():
+    added_to_big_pdf = []
+    # for filepath in islice(glob_dedup.values(), 10):        # use for testing slices
+    for filepath in glob_dedup.values():
         try:
-            writer.append(fr"{str(path)}", import_outline=False)
+            writer.append(fr"{str(filepath)}", import_outline=False)
+            added_to_big_pdf.append(str(filepath.name) + "\n")
         except:
-            print(f"{path.name} is not a nice file. It was not added to big PDF.")
+            print(f"{filepath.name} is not a nice file. It was not added to big PDF.")
             continue
 
     for page in writer.pages:
         page.compress_content_streams(level=9)
     writer.compress_identical_objects(remove_identicals=True, remove_orphans=True)  
 
-    with output_path.open(mode="wb") as output_file:
+    with pdf_output_path.open(mode="wb") as output_file:
         writer.write(output_file)
+
+    contents_output_path = pdf_output_path.parent / f"{pdf_output_path.name}_contents.txt"
+    with contents_output_path.open(mode="w", encoding="utf-8") as output_file:
+        output_file.writelines(added_to_big_pdf)
+    
+    return len(added_to_big_pdf)
 
 if __name__ == "__main__":
     ck_output_path = Path(r"C:\Users\micha\Downloads\ck.pdf")
--- cli.py
+++ cli.py
@@ -5,20 +5,20 @@ from directories import DIRECTORIES
 from big_pdf import create_big_pdf
 
 def get_filename_and_save(input_directory):
+    print("Select save location...")
     output_path = asksaveasfilename(
             defaultextension="pdf",
             filetypes=[("PDF files", "*.pdf"), ("All Files", "*.*")],
     )
     if not output_path :
         return
-    print("Creating PDF. This might take a few minutes...")
-    create_big_pdf(DIRECTORIES[input_directory], Path(output_path))
-    print("PDF created")
+    print("Creating big PDF. This might take a few minutes...")
+    num_files = create_big_pdf(DIRECTORIES[input_directory], Path(output_path))
+    print(f"\nBig PDF created. {num_files} work instructions included.")
 
 input_directory = None
 while not input_directory in DIRECTORIES:
     input_directory = input('\nWhich big PDF do want to create? Enter "ck", "idms", or "tomo": ')
-print("Select save location...")
 get_filename_and_save(input_directory)
 input("Press any key to exit")
 print("Cleaning up...")
