6744e054d02a7395eb61e3ddb91e7966a39154b1
the-chicken-leg <michael.k.koch3@gmail.com>
2024-11-15 12:58:34 +0100

    press Enter to exit

--- cli.py
+++ cli.py
@@ -20,5 +20,5 @@ input_directory = None
 while not input_directory in DIRECTORIES:
     input_directory = input('\nWhich big PDF do want to create? Enter "ck", "idms", or "tomo": ')
 get_filename_and_save(input_directory)
-input("Press any key to exit")
+input("Press Enter key to exit")
 print("Cleaning up...")
