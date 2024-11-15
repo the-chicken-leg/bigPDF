df26b10dbd0f049c8bf8e4aaf81b2a6d50f5a0a9
the-chicken-leg <michael.k.koch3@gmail.com>
2024-11-15 17:31:12 +0100

    refactored big_pdf.py to be more functional

--- big_pdf.py
+++ big_pdf.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 from pypdf import PdfWriter
-# from itertools import islice      # use for testing slices
+from itertools import islice      # use for testing slices
 
 from directories import DIRECTORIES
 
-def create_big_pdf(glob_dir, pdf_output_path):
-    writer = PdfWriter()
-
+def create_writer(glob_dir):
     glob_sort = sorted(
         glob_dir.glob("**/*-SVC-*.pdf"),
         key=lambda filepath: filepath.name
@@ -14,9 +12,10 @@ def create_big_pdf(glob_dir, pdf_output_path):
     glob_dedup = {filepath.name: filepath for filepath in glob_sort}
     glob_dedup.pop("C-SVC-00186 CK Pre-Upgrade Configuration Checklist, 8.x-9.x to 10.x.pdf", None)     # this file destroys everything
 
+    writer = PdfWriter()
     added_to_big_pdf = []
-    # for filepath in islice(glob_dedup.values(), 10):        # use for testing slices
-    for filepath in glob_dedup.values():
+    for filepath in islice(glob_dedup.values(), 10):        # use for testing slices
+    # for filepath in glob_dedup.values():
         try:
             writer.append(fr"{str(filepath)}", import_outline=False)
             added_to_big_pdf.append(str(filepath.name) + "\n")
@@ -24,10 +23,16 @@ def create_big_pdf(glob_dir, pdf_output_path):
             print(f"{filepath.name} is not a nice file. It was not added to big PDF.")
             continue
 
+    return writer, added_to_big_pdf
+
+def compress_writer(writer):
     for page in writer.pages:
         page.compress_content_streams(level=9)
-    writer.compress_identical_objects(remove_identicals=True, remove_orphans=True)  
+    writer.compress_identical_objects(remove_identicals=True, remove_orphans=True)
+    
+    return writer
 
+def write_the_writer(writer, added_to_big_pdf, pdf_output_path):
     with pdf_output_path.open(mode="wb") as output_file:
         writer.write(output_file)
 
@@ -38,10 +43,15 @@ def create_big_pdf(glob_dir, pdf_output_path):
     return len(added_to_big_pdf)
 
 if __name__ == "__main__":
-    ck_output_path = Path(r"C:\Users\micha\Downloads\ck.pdf")
-    idms_output_path = Path(r"C:\Users\micha\Downloads\idms.pdf")
-    tomo_output_path = Path(r"C:\Users\micha\Downloads\tomo.pdf")
+    ck_output_path = Path(r"C:\Users\mkoch\Downloads\ck.pdf")
+    idms_output_path = Path(r"C:\Users\mkoch\Downloads\idms.pdf")
+    tomo_output_path = Path(r"C:\Users\mkoch\Downloads\tomo.pdf")
 
-    create_big_pdf(DIRECTORIES["ck"], ck_output_path)
-    create_big_pdf(DIRECTORIES["idms"], idms_output_path)
-    create_big_pdf(DIRECTORIES["tomo"], tomo_output_path)
+    writer, added_to_big_pdf = create_writer(DIRECTORIES["ck"])
+    write_the_writer(compress_writer(writer), added_to_big_pdf, ck_output_path)
+
+    writer, added_to_big_pdf = create_writer(DIRECTORIES["idms"])
+    write_the_writer(compress_writer(writer), added_to_big_pdf, idms_output_path)
+
+    writer, added_to_big_pdf = create_writer(DIRECTORIES["tomo"])
+    write_the_writer(compress_writer(writer), added_to_big_pdf, tomo_output_path)
